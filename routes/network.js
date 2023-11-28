0f4fb1d4c0f88a74c7a3985bff58a8ce2c520112
Boris Javier Barrera <borisjavier@gmail.com>
2023-11-27 21:33:46 -0300

    Network.js ha sido totalmente modificado para corregir varios errores

--- routes/network.js
+++ routes/network.js
@@ -68,45 +68,61 @@ async function getTransactionDetails(txid, voutIndex, network) {
 router.get('/network/:network/txid/:txid/voutI/:voutIndex', async (req, res) => {
   try {
     const network = req.params.network || req.query.network || req.body.network; //'main'; 
-    const txid = req.params.txid || req.query.txid || req.body.txid; //'a5c5b72267ea32eab1ff4c7a87da1d2c8515ddb260d88c05eb84b2c16e393e48';
-    const voutIndex = req.params.voutIndex || req.query.voutIndex || req.body.voutIndex; // 1;
+  const txid = req.params.txid || req.query.txid || req.body.txid; //'a5c5b72267ea32eab1ff4c7a87da1d2c8515ddb260d88c05eb84b2c16e393e48';
+  const voutIndex = req.params.voutIndex || req.query.voutIndex || req.body.voutIndex; // 1;
+  if (!['main', 'test'].includes(network)) {
+    throw new Error('Red no válida');
+  }
+  if (!/^[a-fA-F0-9]{64}$/.test(txid)) {
+    throw new Error('txid no válido');
+  }
+  if (!Number.isInteger(parseInt(voutIndex)) || parseInt(voutIndex) < 0) {
+    throw new Error('voutIndex no válido');
+  }
+  
+  const url1 = `https://api.whatsonchain.com/v1/bsv/${network}/tx/hash/${txid}`;
+  const res1 = await axios.get(url1);
+  console.log('Respuesta de la consulta en URL1: ', res1.data);//await fetch(url1);
+  //const tx = await res1.json();
+  if (res1.error) {
+    throw new Error(`Error en la respuesta de whatsonchain url1: ${res1.error.message}`);
+  }
 
-    // Validar y sanitizar las entradas
-    if (!['main', 'test'].includes(network)) {
-      throw new Error('Red no válida');
-    }
-    if (!/^[a-fA-F0-9]{64}$/.test(txid)) {
-      throw new Error('txid no válido');
-    }
-    if (!Number.isInteger(parseInt(voutIndex)) || parseInt(voutIndex) < 0) {
-      throw new Error('voutIndex no válido');
-    }
+  if (res1.status !== 200) {
+    throw new Error(`Error en la respuesta de whatsonchain url1: ${res1.status}`);
+  }
+  let tx;
+  try {
+    tx = res1.data;
+  } catch (error) {
+    console.log(await res1.text());
+    throw error; // Lanzar el error para que se maneje en el bloque catch externo
+  }
+  
+  const url2 = `https://api.whatsonchain.com/v1/bsv/${network}/tx/${txid}/${voutIndex}/spent`;
+  let spentTxId;
+  let spent;
+  const res2 = await axios.get(url2);
+  if (res2.error) {
+    throw new Error(`Error en la respuesta de whatsonchain url2: ${res2.error.message}`);
+  }
 
-    const response1 = await axios.get(`https://api.whatsonchain.com/v1/bsv/${network}/tx/hash/${txid}`);
-    console.log('Respuesta de la consulta en URL1: ', response1.data);
-
-    if (response1.error) {
-      throw new Error(`Error en la respuesta de whatsonchain url1: ${response1.error.message}`);
-    }
-
-    if (response1.status !== 200) {
-      throw new Error(`Error en la respuesta de whatsonchain url1: ${response1.status}`);
-    }
-
-    const response2 = await axios.get(`https://api.whatsonchain.com/v1/bsv/${network}/tx/${txid}/${voutIndex}/spent`);
-    console.log('Respuesta de la consulta en URL2: ', response2.data);
-
-    if (response2.error) {
-      throw new Error(`Error en la respuesta de whatsonchain url2: ${response2.error.message}`);
-    }
-
-    if (response2.status !== 200) {
-      throw new Error(`Error en la respuesta de whatsonchain url2: ${response2.status}`);
-    }
-
-    const spentTxId = response2.data.spent.txid;
-    const tx = { txid, voutIndex, spentTxId };
-    res.status(200).json(tx);
+  if (res2.status !== 200) {
+    throw new Error(`Error en la respuesta de whatsonchain url2: ${res2.status}`);
+  }
+  try { 
+    spent = res2.data;
+    console.log('spent: ', spent)
+    spentTxId = spent.txid;
+    console.log('spentTxId: ', spentTxId)
+  } catch (err) {
+    spentTxId = null;
+    console.log(await res2.text());
+  }
+  
+  tx.vout[voutIndex].spentTxId = spentTxId;
+  
+  res.status(200).send(JSON.stringify(tx));
   } catch (error) {
     console.error('Error al llamar a la función primordial getTransactionDetails:', error);
     res.status(500).json({ error: 'Error en el servidor' });
