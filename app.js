ed0cf6be106599d7c9878d0db95b1e77d0fae1d0
Boris Javier Barrera <borisjavier@gmail.com>
2023-11-27 22:13:03 -0300

    Probando con un app simple en el puerto 8080

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const app = express();
+
+app.get('/nombre/:nombre', (req, res) => {
+  const nombre = req.params.nombre || req.query.nombre || req.body.nombre; 
+  const saludo = `Hola ${nombre}`;
+  res.send(saludo);
+});
+
+app.listen(8080, () => {
+  console.log('Servidor API REST escuchando en el puerto indicado');
+});
+
+
+/*const express = require('express');
 const cors = require('cors');
 const app = express();
 const errorHandler = require('./errorhandler');
@@ -13,4 +27,4 @@ app.use(rateLimit);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+});*/
