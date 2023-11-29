1a8550539d6e249c60423c0f4cfb190a083733e5
Boris Javier Barrera <borisjavier@gmail.com>
2023-11-29 17:46:35 -0300

    Habilitando rateLimit error corregido

--- ratelimit.js
+++ ratelimit.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Limitar a 100 solicitudes por hora desde una misma IP
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hora
-  max: 100000
+  max: 100
 });
 
 module.exports = limiter;
