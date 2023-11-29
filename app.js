9038f57124a195979b26be0e416e3d48e26a748c
Boris Javier Barrera <borisjavier@gmail.com>
2023-11-28 22:37:45 -0300

    Sirviendo JSON

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get('/network/:network/txid/:txid/voutI/:voutIndex', async (req, res) => {
   
   tx.vout[voutIndex].spentTxId = spentTxId;
   
-  res.status(200).send(tx);
+  res.status(200).json(tx);
   } catch (error) {
     console.error('Error al llamar a la función primordial getTransactionDetails:', error);
     res.status(500).json({ error: 'Error en el servidor' });
