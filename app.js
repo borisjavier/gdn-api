5f96bb87ff48562115b3ede570302d6ff6c4390c
Boris Javier Barrera <borisjavier@gmail.com>
2024-01-03 16:08:30 -0300

    Fix error returning result from api

--- app.js
+++ app.js
@@ -92,7 +92,8 @@ app.get('/v1/:network/state/:location/', async (req, res) => {
       const tx = response.data;
       //res.status(200).json(tx);
       const hexArray = [tx];
-      res.status(200).json(hexArray);
+      const hex = tx.hex;
+      res.status(200).json(hex);
     } catch (error) {
       console.error('Error al llamar a la url3:', error);
       res.status(500).json({ error: 'Error al llamar a la API externa' });
