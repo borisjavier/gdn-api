7770d35347b197bf3b58c6316cbc59a93b7db7a9
Boris Javier Barrera <borisjavier@gmail.com>
2024-01-09 10:49:17 -0300

    Change rate limit

--- ratelimit.js
+++ ratelimit.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Middleware para aplicar el límite de solicitudes por IP
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hora
-  max: 10000, // máximo de 100 solicitudes por ventana
+  max: 100000, // máximo de 100 solicitudes por ventana
   keyGenerator: (req) => {
     if (req.originalUrl === process.env.GN_APP_URL) {
       return false; 
