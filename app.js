c7d8787e2d3556086c4d6dd1f825c5e1aecbe465
Boris Javier Barrera <borisjavier@gmail.com>
2026-01-11 22:37:49 -0300

    Threshold to 10. Cleaned

--- app.js
+++ app.js
@@ -458,7 +458,7 @@ app.post('/update-balance', async (req, res) => {
                 }
 
                 // VERIFICACIÓN PARA EL RECEPTOR
-                if (newRecCount >= 5 && tr.uidReceptor) {
+                if (newRecCount >= 10 && tr.uidReceptor) {
                     addressesToValidate.push({ uid: tr.uidReceptor, address: tr.receptor });
                     newRecCount = 0; // Reiniciamos contador en DB porque dispararemos validación
                 }
@@ -493,7 +493,7 @@ app.post('/update-balance', async (req, res) => {
             }
 
             // VERIFICACIÓN PARA EL EMISOR
-            if (newEmiCount >= 5 && uidEmisor) {
+            if (newEmiCount >= 10 && uidEmisor) {
                 addressesToValidate.push({ uid: uidEmisor, address: emisor });
                 newEmiCount = 0; 
             }
